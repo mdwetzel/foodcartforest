8ae88ddbf8b7c737e9b5c64ae9270f3a9dd570f9
Mark Wetzel <mark@markwetzel.com>
2013-07-19 16:16:39 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodcartforest::Application.config.secret_key_base = '22abff59fbbcac32122515208be02cfe1eeea14ed77b4dc701acb01ec413056261cd8503fd30cfb8aee12a74e2c167adf7edbedaf8f0c989eeb7fc7aeeb200d8'
